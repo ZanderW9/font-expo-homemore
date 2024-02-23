f7d66de00ff0bfece0efcf56e11436a1e25ce580
MarcHJL <46721809+MarcHJL@users.noreply.github.com>
2024-02-23 21:49:49 +1100

    change background url (#141)

--- config/s3.tsx
+++ config/s3.tsx
@@ -19,18 +19,3 @@ export const uploadImage = async (signedUrl, fileType, asset) => {
     console.error("Error uploading image:", error);
   }
 };
-
-export const fetchDynamicUrl = async () => {
-  try {
-    const response = await fetch(
-      `${process.env.EXPO_PUBLIC_S3_URL}/${process.env.EXPO_PUBLIC_DEVELOPER}`,
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data: ", error);
-  }
-};
