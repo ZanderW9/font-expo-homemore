0aec039e8862dfe9a09bebdbb7880593d0393793
MarcHJL <46721809+MarcHJL@users.noreply.github.com>
2024-03-15 16:57:28 +1100

    New build number (#168)

---
+++ constants/Translations.ts
@@ -0,0 +1,11 @@
+const Translations = {
+  en: {
+    locationPermission:
+      "Allow $(PRODUCT_NAME) to access your location to show nearby properties and provide personalized recommendations.",
+  },
+  zh: {
+    locationPermission:
+      "允许 $(PRODUCT_NAME) 访问您的位置以显示附近的房产并提供个性化推荐。",
+  },
+};
+export default Translations;
